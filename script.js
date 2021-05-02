6b1b5f2df97a8e9a074ca0372b01dfa141576125
brettherbst <brettherbst1@gmail.com>
2021-05-02 05:08:28 -0700

    Part 1 of lab

--- script.js
+++ script.js
@@ -1,16 +1,166 @@
 // script.js
 
-const img = new Image(); // used to load image from <input> and draw to canvas
 
+/* Bullet 1- const vars */
+const img = new Image(); // used to load image from <input> and draw to canvas
+const canvas = document.getElementById('user-image');
+const rect = canvas.getContext('2d');
+const genMeme = document.getElementById('generate-meme');
+
+/* slider for volume */
+const volumeGroup = document.getElementById('volume-group');
+const inputSlider = volumeGroup.querySelectorAll('input')[0];
+const volume = volumeGroup.querySelectorAll('img')[0];
+
+
+/* Bulllet 2/3 - const vars*/
+const inFile = document.getElementById('image-input');
+
+/* Buttons */
+const clearButton = document.getElementById('button-group').querySelectorAll('button')[0];
+const submit = genMeme.querySelectorAll('button')[0];
+const inText = document.getElementById('button-group').querySelectorAll('button')[1];
+
+/* voices stuff */
+const synth = window.speechSynthesis;
+var voices = [];
+const voiceSelection = document.getElementById('voice-selection');
+
+
+/* Bullet 1 implementation */
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
+  // Clearing canvas context
+  rect.clearRect(0,0, canvas.width, canvas.height);
+
+  // filling w/ black
+  rect.fillStyle = 'black';
+  rect.fillRect(0,0,canvas.width,canvas.height);
+  //clearing form
+  genMeme.reset();
+
+  //grab dimensions
+  var dims = getDimmensions(canvas.width,canvas.height,img.width,img.height);
+
+  //drawing image
+  rect.drawImage(img, dims.startX,dims.startY,dims.width,dims.height);
 
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
+
+/* Bulllet 2 */
+inFile.addEventListener('change', files);
+function files(){
+
+  let newImg = inFile.files[0];
+
+  //creates url than can then be used to store in src
+  img.src = URL.createObjectURL(newImg);
+
+  //setting image name by extracting the image file name from the file path
+  img.alt = newImg.name;
+};
+
+/* Function used in Bullet 3 */
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if (voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelection.appendChild(option);
+  }
+}
+
+genMeme.addEventListener('submit', (event) => {
+  var topT = document.getElementById('text-top').value;
+  var bottomT = document.getElementById('text-bottom').value;
+
+  
+  rect.textAlign = 'center';
+  rect.font = '70px Comic Sans';
+  rect.fillStyle = 'black';
+
+  
+  rect.fillText(topT, canvas.width / 2, canvas.height / 16);
+  rect.fillText(bottomT, canvas.width / 2, canvas.height * .9);
+
+
+
+  submit.disabled = true;
+  clearButton.disabled = false;
+  inText.disabled = false;
+  voiceSelection.disabled = false;
+  populateVoiceList();
+  event.preventDefault();
+});
+
+
+
+/* Bullet 4 */
+clearButton.addEventListener('click', () => {
+  // clears text
+  rect.clearRect(0, 0, canvas.width, canvas.height);
+  //resets generate meme
+  genMeme.reset();
+
+
+  //adjusts buttons
+  inText.disabled = true;
+  clearButton.disabled = true;
+  submit.disabled = false;
+});
+
+/* Bullet 6 */
+inText.addEventListener('click', () => {
+  var speakTop = new SpeechSynthesisUtterance(document.getElementById('text-top').value);
+  var speakBottom = new SpeechSynthesisUtterance(document.getElementById('text-bottom').value);
+
+  var selectedOption = voiceSelection.selectedOptions[0].getAttribute('data-name');
+
+  for (var i = 0; i < voices.length ; i++) {
+    if (voices[i].name === selectedOption) {
+      speakTop.voice = voices[i];
+      speakBottom.voice = voices[i];
+    }
+  }
+  speakTop.volume = inputSlider.value / 100;
+  synth.speak(speakTop);
+  speakBottom.volume = inputSlider.value / 100;
+  synth.speak(speakBottom);
+})
+
+/* Bullet 7 */
+volumeGroup.addEventListener('input', () => {
+
+  if (inputSlider.value > 66) {
+    volume.src = 'icons/volume-level-3.svg';
+    volume.alt = 'Level 3';
+  }
+  else if (inputSlider.value <= 66 && inputSlider.value >= 34) {
+    volume.src = 'icons/volume-level-2.svg';
+    volume.alt = 'Level 2';
+  }
+  else if (inputSlider.value < 34 && inputSlider.value != 0) {
+    volume.src = 'icons/volume-level-1.svg';
+    volume.alt = 'Level 1';
+  }
+  else{
+    volume.src = 'icons/volume-level-0.svg';
+    volume.alt = 'Level 0';
+  }
+});
+
 
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
